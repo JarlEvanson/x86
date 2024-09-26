d65e6ff4235bf371dc9ebfaf3a1d43d3067d32b2
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-26 18:24:17 -0500

    Add support for the `rdseed` instruction.

--- src/instructions/random.rs
+++ src/instructions/random.rs
@@ -107,3 +107,107 @@ impl RdRand {
         Some(rand)
     }
 }
+
+/// Interface to the `rdseed` instruction, which is a hardware RNG.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct RdSeed(());
+
+impl RdSeed {
+    /// Creates a new [`RdSeed`].
+    ///
+    /// If the `rdseed` instruction is not supported, then this function returns [`None`].
+    pub fn new() -> Option<Self> {
+        if !has_cpuid() {
+            return None;
+        }
+
+        // SAFETY:
+        // CPUID is supported.
+        let cpuid_ebx = unsafe { cpuid(7, 0).ebx };
+
+        #[allow(clippy::nonminimal_bool)]
+        if !(cpuid_ebx & (1 << 18) == (1 << 18)) {
+            return None;
+        }
+
+        Some(Self(()))
+    }
+
+    /// Creates a new [`RdRand`] without checking if the `rdseed` instruction is supported.
+    ///
+    /// # Safety
+    /// This processor must support the `rdseed` instruction.
+    pub unsafe fn new_unchecked() -> Self {
+        Self(())
+    }
+
+    /// Returns a random 16-bit number from `rdseed`.
+    pub fn get_u16(self) -> Option<u16> {
+        let mut rand: u16;
+        let success: u8;
+
+        // SAFETY:
+        // The `rdseed` instruction is supported.
+        unsafe {
+            asm!(
+                "rdseed {:x}",
+                "setc {}",
+                lateout(reg) rand,
+                lateout(reg_byte) success,
+            )
+        }
+
+        if success == 0 {
+            return None;
+        }
+
+        Some(rand)
+    }
+
+    /// Returns a random 16-bit number from `rdseed`.
+    pub fn get_u32(self) -> Option<u32> {
+        let mut rand: u32;
+        let success: u8;
+
+        // SAFETY:
+        // The `rdseed` instruction is supported.
+        unsafe {
+            asm!(
+                "rdseed {:e}",
+                "setc {}",
+                lateout(reg) rand,
+                lateout(reg_byte) success,
+            )
+        }
+
+        if success == 0 {
+            return None;
+        }
+
+        Some(rand)
+    }
+
+    /// Returns a random 16-bit number from `rdseed`.
+    #[cfg(target_arch = "x86_64")]
+    pub fn get_u64(self) -> Option<u64> {
+        let mut rand: u64;
+        let success: u8;
+
+        // SAFETY:
+        // The `rdseed` instruction is supported.
+        unsafe {
+            asm!(
+                "rdseed {}",
+                "setc {}",
+                lateout(reg) rand,
+                lateout(reg_byte) success,
+            )
+        }
+
+        if success == 0 {
+            return None;
+        }
+
+        Some(rand)
+    }
+}
