e36a8887cb0877986e58f4fbf8df95a5d3d7f5e0
Jarl Evanson <evanson.jarl@gmail.com>
2024-09-28 02:13:18 -0500

    Add features to enable reduction of API surface

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,24 @@
 
 #![cfg_attr(not(test), no_std)]
 
+// Ensure that at least one of the architecture is enabled.
+#[cfg(not(any(feature = "x86", feature = "x86_64")))]
+compile_error!("At least one of `x86` and `x86_64` must be enabled");
 
+// Ensure that if feature `instructions` is enabled, the target architecture is one of the
+// supported architectures.
+#[cfg(all(
+    feature = "instructions",
+    not(any(target_arch = "x86", target_arch = "x86_64",))
+))]
+compile_error!("Feature `instructions` mandates compilation for a supported architecture");
+
+// Ensure that if feature `instructions` is enabled, the target architecture's feature is enabled.
+#[cfg(all(
+    feature = "instructions",
+    not(any(
+        all(target_arch = "x86", feature = "x86"),
+        all(target_arch = "x86_64", feature = "x86_64")
+    ))
+))]
+compile_error!("Feature `instructions` mandates target architecture's feature is enabled");
